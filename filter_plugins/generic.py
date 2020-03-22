951462ad2eb2e2961877f2261397af0e4d0c2770
Jakub Kaláb <jakubkalab@gmail.com>
2020-03-22 19:36:42 +0100

    simplified pacemaker clustering logic
    - instead of trying every combination of role and network group,
      define pacemaker_clusters explicitly and iterate over them
    - each cluster has a name, role_group, network_group and resources
    
    added support for nginx pacemaker resource
    - added tasks to manage the resource
    - added status page vhost to nginx_setup role
    - crated testing vhost

--- filter_plugins/generic.py
+++ filter_plugins/generic.py
@@ -2,6 +2,10 @@ from ansible.plugins.filter.ipaddr import ipaddr
 
 class FilterModule:
   @staticmethod
+  def split(string, *args, **kwargs):
+    return string.split(*args, **kwargs)
+
+  @staticmethod
   def bool_to_str(bool_val, yes_str = 'yes', no_str = 'no'):
     return yes_str if bool_val else no_str
 
@@ -10,6 +14,6 @@ class FilterModule:
     return ipaddr(f"{iface['ipv4']['network']}/{iface['ipv4']['netmask']}", 'net')
 
   def filters(self):
-    return {'bool_to_str':          self.bool_to_str,
+    return {'split':                self.split,
+            'bool_to_str':          self.bool_to_str,
             'iface_to_subnet_cidr': self.iface_to_subnet_cidr}
-    return { 'bool_to_str': bool_to_str }
